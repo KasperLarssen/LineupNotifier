cf506ec0c37cae5395ee9c2542aab064e0584506
Kasper Rolf Larssen <kasper.larssen@gmail.com>
2020-06-01 14:56:46 +0200

    trying to fix bug

--- FlashscoreScrape.py
+++ FlashscoreScrape.py
@@ -4,14 +4,17 @@ from playsound import playsound
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
 
 webdriver = './chromedriver.exe'
-club = input('Vælg et superligahold: ')
+league = input('Vælg en liga: ')
+club = input('Vælg et hold: ')
 driver = Chrome(webdriver)
 url = "http://flashscore.dk"
 driver.get(url)
+# driver.minimize_window()
 # Click the specified league tab
-league = 'Superliga'
 time.sleep(2)
 league_link = driver.find_element_by_link_text(league)
 league_link.click()
@@ -28,21 +31,21 @@ for x in match_links:
 if(not clubfound):
     print("Det indtastede klubnavn kunne ikke findes. Prøv igen")
     sys.exit()
-# INSERT if statement find FCK
-# parentElement = driver.find_element_by_class_name("bar")
-# elementList = parentElement.find_elements_by_tag_name("li")
-# match_links[0].click()
+
 
 # click the lineup fan, if present. Otherwise try again in 5 seconds.
 time.sleep(2)
-window_after = driver.window_handles[1]
-driver.switch_to_window(window_after)
-
+#window_after = driver.window_handles[1]
+#driver.switch_to_window(window_after)
 
 def scanForLineup():
     try:
         print("Scanning for lineups...")
+        #doesn't work right now. cant find the Opstilling
         lineup_link = driver.find_element_by_link_text('Opstilling')
+        # lineup_link = driver.find_element_by_xpath("*//*[contains(text(),'Opstilling')]")
+
+
         lineup_link.click()
         playsound('ready.mp3')
         print("success!")
